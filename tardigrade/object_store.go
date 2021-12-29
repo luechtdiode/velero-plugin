ec4cc074ca698de8293b191287d6eb2471e00348
Roland Seidel <register@interpolar.ch>
2021-12-29 22:10:59 +0100

    .

--- tardigrade/object_store.go
+++ tardigrade/object_store.go
@@ -47,13 +47,15 @@ func NewObjectStore(logger logrus.FieldLogger) *ObjectStore {
 //   - accessGrant (required): serialized access grant to Tardigrade project.
 func (o *ObjectStore) Init(config map[string]string) error {
 	o.log.Debug("objectStore.Init called")
-
+	var err
+	
 	o.log.Debug("Getting accessGrant from environment")
 	grant := os.Getenv(accessGrant)
+	
     if grant != "" {
 		o.log.Debug("accessGrant from environment found")
     } else {
-		err := veleroplugin.ValidateObjectStoreConfigKeys(config, accessGrant)
+		err = veleroplugin.ValidateObjectStoreConfigKeys(config, accessGrant)
 		if err != nil {
 			return err
 		}
